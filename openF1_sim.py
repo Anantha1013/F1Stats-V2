60c13d28889c721a0df1f1406fea8b208cac332d
Anantha 1013 <nmananthalakshmy@gmail.com>
2026-05-22 09:41:04 +0530

    Minor changes

--- openF1_sim.py
+++ openF1_sim.py
@@ -27,15 +27,16 @@ drivers_details=json.loads(response.read().decode('utf-8'))
 #retrieving session telemetry
 url=f'https://api.openf1.org/v1/location?session_key={session_key}&driver_number=1' #for testing , I only have included driver number 1
 response=urlopen(url)
-telemetry_data=json.loads(response.read().decode('utf-8')) #updates -> every 4 minutes 
+telemetry_data=json.loads(response.read().decode('utf-8')) #
 print(telemetry_data[0])
 
-#TODO- simulate the race using the telemetry obtained
-
-
+#TODO- simulate the race using the telemetry obtained - Filter required fields - Send to Red Panda stream?
+for tel_entry in telemetry_data:
+    print(tel_entry)
+    time.delay(0.27) #Open F1 updates telemetry every 0.27 seconds
 
 #intervals
-url=f'https://api.openf1.org/v1/intervals?session_key={session_key}&driver_number=1'
+url=f'https://api.openf1.org/v1/intervals?session_key={session_key}&driver_number=1' #updates -> every 4 minutes 
 response=urlopen(url)
 interval_from_lead=json.loads(response.read().decode('utf-8'))
 # print(interval_from_lead)
