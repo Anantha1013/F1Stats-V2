ae24b7682b885b7bd8fe8c8e3915e12e9f6c7a13
Anantha 1013 <nmananthalakshmy@gmail.com>
2026-05-22 08:13:07 +0530

    Tested Open F1 Api endpoints and retrieved requited fields

---
+++ openF1_sim.py
@@ -0,0 +1,41 @@
+from urllib.request import urlopen
+import json
+import time
+
+import urllib
+
+#retrieving session
+filters={
+    "year":2024,
+    "country_name":"Japan",
+    "session_name":"Race"
+}
+query_string = urllib.parse.urlencode(filters)
+url=f'https://api.openf1.org/v1/sessions?{query_string}'
+responce=urlopen(url)
+session_data=json.loads(responce.read().decode('utf-8'))
+# print(session_data)
+
+
+#retrieve teams and drivers participating in the session
+session_key=session_data[0]['session_key']
+url=f'https://api.openf1.org/v1/drivers?session_key={session_key}'
+response=urlopen(url)
+drivers_details=json.loads(response.read().decode('utf-8'))
+# print(drivers_details)
+
+#retrieving session telemetry
+url=f'https://api.openf1.org/v1/location?session_key={session_key}&driver_number=1' #for testing , I only have included driver number 1
+response=urlopen(url)
+telemetry_data=json.loads(response.read().decode('utf-8')) #updates -> every 4 minutes 
+print(telemetry_data[0])
+
+#TODO- simulate the race using the telemetry obtained
+
+
+
+#intervals
+url=f'https://api.openf1.org/v1/intervals?session_key={session_key}&driver_number=1'
+response=urlopen(url)
+interval_from_lead=json.loads(response.read().decode('utf-8'))
+# print(interval_from_lead)
